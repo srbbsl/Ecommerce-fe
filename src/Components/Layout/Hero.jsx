a2cac5cbe199b72c0ff39c19f255694228840454
srbbsl <saurabbsl@gmail.com>
2025-06-06 19:53:30 +0545

    changes

--- src/Components/Layout/Hero.jsx
+++ src/Components/Layout/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
         <img 
             src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Ecommerce"
-            className="w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover"
+            className="w-full h-[100px] md:h-[200px] lg:h-[600px] object-cover"
         />
         <div className="absolute inset-0 bg-black/5 flex items-center justify-center">
             <div className="text-white text-center p-6">
@@ -15,7 +15,7 @@ export const Hero = () => {
                 </h1>
 
                 <p className="text-xl md:text-lg mb-6 font-semibold">
-                    Explore our outfits with fast delivery.
+                    Explore our outfits with fast delivery
                 </p>
                 <Link to='#' className="bg-white text-black px-6 py-2 rounded-sm txt-lg font-semibold">
                     Shop Now
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Hero } from "../Components/Layout/Hero"
+import { GenderCollection } from "../Components/Products/GenderCollection"
 
 export const Home = () => {
   return (
     <div>
         <Hero />
+        <GenderCollection />
     </div>
   )
 }
