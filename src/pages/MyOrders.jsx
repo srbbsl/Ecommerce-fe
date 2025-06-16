d7e5f9b22a3c032e4836668b17f87b5be470775e
srbbsl <saurabbsl@gmail.com>
2025-06-16 17:14:08 +0545

    3:47:11

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -41,7 +41,7 @@ export const MyOrders = () => {
     });
 
   return (
-    <div className="border-2 border-black max-w-7xl mx-auto p-4 sm:p-6">
+    <div className="max-w-7xl mx-auto p-4 sm:p-6">
         <h2
             className="text-xl sm:text-2xl fond-bold mb-6"
         >
@@ -97,7 +97,7 @@ export const MyOrders = () => {
                                 </td>
 
                                 <td className="py-2 px-2 sm:py-4 sm:px-4">
-                                    {order.totalPrice}
+                                    NPR {order.totalPrice}
                                 </td>
 
                                 <td className="py-2 px-2 sm:py-4 sm:px-4">
@@ -106,7 +106,7 @@ export const MyOrders = () => {
                                             order.isPaid
                                             ? 'bg-green-100 text-green-700'
                                             : 'bg bg-red-100 text-red-700'
-                                        }`}
+                                        } px-2 py-1 rounded-full text-xs sm:text-sm font-medium`}
                                     >
                                         {order.isPaid ? 'Paid' : 'Pending'}
                                     </span>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,18 +2,18 @@ import { MyOrders } from "./MyOrders"
 
 export const Profile = () => {
   return (
-    <div className="border-5 border-red-600 flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
 
-      <div className="border-4 border-green-400 flex-grow p-4 md:p-6">
+      <div className="flex-grow container mx-auto p-4 md:p-6">
 
-        <div className="border-3 border-purple-500 flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0">
+        <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0">
 
           {/* Left Section */}
           <div className="md:w-1/3 lg:w-1/4 shadow-xl rounded-lg p-6">
             <h1 className="text-2xl md:text-3xl font-bold">John Doe</h1>
             <p className="text-lg text-gray-600">John@example.com</p>
             <button
-              className="bg-red-500 w-full text-white py-2 px-4 rounded hover:bg-red-600"
+              className="bg-red-500 w-full text-white py-1 px-4 rounded hover:bg-red-600 font-medium"
             >
                 Logout
             </button>
